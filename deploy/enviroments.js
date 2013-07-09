3f12c219ba0d16548f0f095fecbe2b528d645623
atomsfat <atomsmail@gmail.com>
2013-07-09 18:50:18 -0500

    the configuration was wrong

--- deploy/enviroments.js
+++ deploy/enviroments.js
@@ -5,36 +5,36 @@
       "name": "dev",
       "desc": "dev enviroment",
       "config": {
-        "apiUrl": "https://apiqa.winbits.com/v1",
+        "apiUrl": "http://apiqa.winbits.com/v1",
         "baseUrl": "http://widgets.winbits.com/qa",
-        "loginRedirectUrl": "http://widgets/winbits.com/qa/ilogin.html"
+        "loginRedirectUrl": "http://widgets.winbits.com/qa/ilogin.html"
       }
     },
     {
       "name": "qa",
       "desc": "qa enviroment",
       "config": {
-        "apiUrl": "https://apiqa.winbits.com/v1",
+        "apiUrl": "http://apiqa.winbits.com/v1",
         "baseUrl": "http://widgets.winbits.com/qa",
-        "loginRedirectUrl": "http://widgets/winbits.com/qa/ilogin.html"
+        "loginRedirectUrl": "http://widgets.winbits.com/qa/ilogin.html"
       }
     },
     {
       "name": "staging",
       "desc": "stagin enviroment",
       "config": {
-        "apiUrl": "https://api.winbits.com/v1",
+        "apiUrl": "http://api.winbits.com/v1",
         "baseUrl": "http://widgets.winbits.com/prod",
-        "loginRedirectUrl": "http://widgets/winbits.com/prod/ilogin.html"
+        "loginRedirectUrl": "http://widgets.winbits.com/prod/ilogin.html"
       }
     },
     {
       "name": "prod",
       "desc": "production enviroment",
       "config": {
-        "apiUrl": "https://api.winbits.com/v1",
+        "apiUrl": "http://api.winbits.com/v1",
         "baseUrl": "http://widgets.winbits.com/prod",
-        "loginRedirectUrl": "http://widgets/winbits.com/prod/ilogin.html"
+        "loginRedirectUrl": "http://widgets.winbits.com/prod/ilogin.html"
       }
     }
   ]
