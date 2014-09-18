9e8e51a303d7e1c327d27501e4c7b4dddc9b1ba1
jluis <jorge.moreno@clickonero.com.mx>
2014-09-18 13:13:11 -0500

    fix bug in flow of transfer virtual cart when present any error and have valid items

--- widget/vendor/scripts/script.js
+++ widget/vendor/scripts/script.js
@@ -779,61 +779,61 @@ jQuery.fn.imageError = function(options){
 //      FANCYBOX: Modales con FancyBox
 // ++++++++++++++++++++++++++++++++++++++
 
-jQuery.fn.fancyBox = function(options){
-  var defaults = $.extend({
-        afterShow: false
-      }, options),
-      optionsFancybox = {},
-      defaultFancybox = function(obj){
-        optionsFancybox = {
-          padding: 0,
-          margin: 0,
-          width: $(obj).data('fancyboxwidth'),
-          type: 'ajax',
-          afterShow: defaults.afterShow
+  jQuery.fn.fancyBox = function(){
+    var optionsFancybox = {},
+        defaultFancybox = function(obj){
+          optionsFancybox = {
+            padding: 0,
+            margin: 0,
+            width: $(obj).data('fancyboxwidth'),
+            iframe: {
+              scrolling: 'auto',
+              preload: true
+            },
+            type: 'iframe'
+          };
+        },
+        hrefFancybox = function(obj) {
+          optionsFancybox = {
+            padding: 0,
+            margin: 0,
+            width: $(obj).data('fancyboxwidth'),
+            href: $(obj).data('fancyboxhref'),
+            iframe: {
+              scrolling: 'auto',
+              preload: true
+            },
+            type: 'iframe'
+          };
+        },
+        noCloseFancybox = function(obj){
+          optionsFancybox = {
+            padding: 0,
+            margin: 0,
+            closeBtn: false,
+            width: $(obj).data('fancyboxwidth'),
+            href: $(obj).data('fancyboxhref'),
+            iframe: {
+              scrolling: 'auto',
+              preload: true
+            },
+            type: 'iframe'
+          };
         };
-      },
-      hrefFancybox = function(obj) {
-        optionsFancybox = {
-          padding: 0,
-          margin: 0,
-          width: $(obj).data('fancyboxwidth'),
-          href: $(obj).data('fancyboxhref'),
-          iframe: {
-            scrolling: 'auto',
-            preload: true
-          },
-          type: 'iframe',
-          afterShow: defaults.afterShow
-        };
-      },
-      noCloseFancybox = function(obj){
-        optionsFancybox = {
-          padding: 0,
-          margin: 0,
-          closeBtn: false,
-          width: $(obj).data('fancyboxwidth'),
-          href: $(obj).data('fancyboxhref'),
-          iframe: {
-            scrolling: 'auto',
-            preload: true
-          },
-          type: 'iframe'
-        };
-      };
-  return this.each(function(){
-    if ($(this).data('fancyboxhref')){
-      hrefFancybox(this);
-      $(this).fancybox(optionsFancybox);
-    } else if ($(this).data('fancybox-noclosebtn')){
-      noCloseFancybox(this);
-      $(this).fancybox(optionsFancybox);
-    } else {
-      defaultFancybox(this);
-      $(this).fancybox(optionsFancybox);
-    }
-  });
-};
+    return this.each(function(){
+      if ($(this).data('fancyboxhref')){
+        hrefFancybox(this);
+        $(this).fancybox(optionsFancybox);
+      } else if ($(this).data('fancybox-noclosebtn')){
+        noCloseFancybox(this);
+        $(this).fancybox(optionsFancybox);
+      } else {
+        defaultFancybox(this);
+        $(this).fancybox(optionsFancybox);
+      }
+    });
+  };
+
 
 /* **********************************************
  Begin mailingMenuCheckboxs.js
