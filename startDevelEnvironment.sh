9c0c7c2070db033a2aae3848b751186ec13957ab
= <sergi.fernandez@apium.com>
2015-09-02 18:40:02 +0200

    fixing nginx configuration adding certificate info

--- startDevelEnvironment.sh
+++ startDevelEnvironment.sh
@@ -4,13 +4,12 @@ set -e
 set -x
 
 #DOWNLOAD PACKAGES
-make install
+cd widget && rm npm-debug.log && rm -fr bower_components && rm -fr node_modules && rm -fr public && sudo -u $SUDO_USER npm install && cd ..
+cd checkout && rm npm-debug.log && rm -fr bower_components && rm -fr node_modules && rm -fr public && sudo -u $SUDO_USER npm install && cd ..
+cd api && rm npm-debug.log && rm -fr bower_components && rm -fr node_modules && npm rm -fr public && sudo -u $SUDO_USER npm install && cd ..
 
-#BUILD AND WATCH
-
-#make watch_widget &
-#make watch_checkout &
-#make watch_api &
+sudo -u $SUDO_USER make install
+sudo -u $SUDO_USER make build
 
 #START ENVIRONMENT
 sudo docker-compose kill
